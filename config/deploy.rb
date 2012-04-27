7da01e149176fdbc7707adacbf5a9e3ad1817051
Aaron Glenn <aaron@tout.com>
2012-04-27 13:02:29 -0700

    deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "beautifulcode"
+set :repository,  "git@github.com/beautifulcode/beautifulcode"
+
+set :scm, :git
+
+set :domain, "beautifulco.de"
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
