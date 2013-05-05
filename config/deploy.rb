f648d07a79fea3ca89106afc645928fb72bcd83c
Aaron Glenn <aaron@aaronglenn.ca>
2013-05-05 13:30:15 -0700

    committing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,18 @@
-set :application, "beautifulcode"
-set :repository,  "git@github.com/beautifulcode/beautifulcode"
+require 'bundler/capistrano'
+require 'rvm/capistrano'
 
+set :rvm_ruby_string, '1.9.3'
+
+set :application, "beautifulco.de"
+set :repository,  "git@github.com:beautifulcode/beautifulcode.git"
+
+set :user, 'deploy'
+set :use_sudo, false
 set :scm, :git
 
-set :domain, "beautifulco.de"
+set :deploy_to, "/var/www/apps/#{application}"
+
+set :domain, "198.199.104.225"
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
